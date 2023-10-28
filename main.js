52ff1c550499ec3856ff04227107184bf48dc94c
Alejandra Bravo <148171181+AlejandraB99@users.noreply.github.com>
2023-10-28 13:27:48 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
+ cursor: "<span style='color: #5a9b95;'>|</span>",
 });
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color: #fb4993;">Estudio la licenciatura en Informática Administrativa y soy del Estado de México.</span>')
+  .typeString('<span style="color: #44878f;">Estudio la licenciatura en Informática Administrativa y soy del Estado de México.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
